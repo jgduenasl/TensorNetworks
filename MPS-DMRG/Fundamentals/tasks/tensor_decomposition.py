6ba255c34a87870f33ce04c7d5124a6115a97ac3
jgduenasl <jgduenasl@unal.edu.co>
2020-01-28 22:33:46 -0500

    Create tensor_decomposition.py

---
+++ MPS-DMRG/Fundamentals/tasks/tensor_decomposition.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#
+# Author: DP 
+# Mainteiner: DP & JG
+# Copyright: PhysMLGr, 2020, GLP-3 or later
+#===========================================
+
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensornetwork
+from tensornetwork import ncon
+net = tensornetwork.TensorNetwork()
+sess = tf.InteractiveSession()
