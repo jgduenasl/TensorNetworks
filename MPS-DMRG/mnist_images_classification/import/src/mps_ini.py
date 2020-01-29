4203292efac10baa5f39c1773d3c0f4e5dd1f11e
jgduenasl <jgduenasl@unal.edu.co>
2020-01-29 10:12:08 -0500

    Update mps_ini.py

--- MPS-DMRG/mnist_images_classification/import/src/mps_ini.py
+++ MPS-DMRG/mnist_images_classification/import/src/mps_ini.py
@@ -4,6 +4,8 @@
 # Maintainer: DP & JG
 # Coypright:
 #=============================
+#/mnist_images_classification/import/src/mps_ini.py
+#
 
 import numpy as np
 import numpy.linalg as LA
@@ -21,15 +23,3 @@ A[0] = np.random.rand(1,chid,chi)
 A[Nsites-1] = np.random.rand(chi,chid,1,label) 
 for k in range(1,Nsites-1):
     A[k] = np.random.rand(chi,chid,chi)
-    
-#Left canonical form
-for p in range(Nsites-1):
-    chil = A[p].shape[0] ; chir = A[p].shape[2];
-    Q,R= LA.qr(A[p].reshape(chil*chid,chir));
-    A[p] = Q.reshape(chil,chid,Q.shape[1])
-    if p < Nsites - 2:
-        A[p+1] = oe.contract('ij,jkn->ikn',R/LA.norm(R),A[p+1])
-    else:
-        A[p+1] = oe.contract('ij,jknl->iknl',R/LA.norm(R),A[p+1])
-        
-np.allclose(oe.contract('ijk,ijn->kn',A[194],A[194]),np.eye(A[194].shape[2]))
